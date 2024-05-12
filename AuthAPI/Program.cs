3f707b42fc6dc59de8e843dc2cbf1fd39e5389f2
rhr190 <rafsanhabib97@gmail.com>
2024-05-12 16:58:21 +0600

    added configuration details and libraries

--- AuthAPI/Program.cs
+++ AuthAPI/Program.cs
@@ -1,4 +1,10 @@
+using AuthAPI.Data;
+using AuthAPI.Models;
+using AuthAPI.Services;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -10,6 +16,35 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Context>(Options =>
+{
+    Options.UseSqlServer(builder.
+    Configuration.
+    GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<JWTService>();
+
+// defining our identity core service
+builder.Services.AddIdentityCore<User>(options =>{
+
+    //for password requirement
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+
+    // for email confirmation
+    options.SignIn.RequireConfirmedEmail = true;
+})  
+    .AddRoles<IdentityRole>() // add roles
+    .AddRoleManager<RoleManager<IdentityRole>>() // make use of role manager
+    .AddEntityFrameworkStores<Context>() // providing DBContext
+    .AddSignInManager<SignInManager<User>>() // make use of signin manager
+    .AddUserManager<UserManager<User>>() // make use of user manager
+    .AddDefaultTokenProviders(); // create token for email confirmation
 
 var app = builder.Build();
 
